4b34036c41372fe392622cf620386c85d0194a4b
MatiCanzani <matiascanzani@gmail.com>
2024-01-10 13:25:44 +0000

    first commit

---
+++ tailwind.config.mjs
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+export default {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        chalet: [
+          "ChaletNewYorkNineteenSeventy",
+          ...defaultTheme.fontFamily.sans,
+        ],
+        Cinematografica: ["Cinematografica", ...defaultTheme.fontFamily.sans],
+        Impact: ["Impact", ...defaultTheme.fontFamily.sans],
+        MyriadPro: ["MyriadPro", ...defaultTheme.fontFamily.sans],
+        compactaBT: ["compactaBT", ...defaultTheme.fontFamily.sans],
+        FormaDJRMicro: ["FormaDJRMicro", ...defaultTheme.fontFamily.sans],
+      },
+      backgroundImage: {
+        pattern: "url('./src/images/back.svg')",
+      },
+    },
+    colors: {
+      "deep-blue": "#121926",
+      gold: "#FFD971",
+      "light-gold": "#FCEDCD",
+    },
+  },
+  plugins: [],
+};
