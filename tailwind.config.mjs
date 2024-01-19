af808f91cd50d8d02949a6f1618139afbdd53000
MatiCanzani <matiascanzani@gmail.com>
2024-01-19 01:40:08 +0000

    Add responsive design and images

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -16,12 +16,23 @@ export default {
         compactaBT: ["compactaBT", ...defaultTheme.fontFamily.sans],
         FormaDJRMicro: ["FormaDJRMicro", ...defaultTheme.fontFamily.sans],
       },
+      backgroundImage: {
+        'event-1-desktop': "url('/src/images/works/Desktop_1.png')",
+        'event-3-desktop': "url('/src/images/works/Desktop_3.png')",
+        'event-2-desktop': "url('/src/images/works/Desktop_2.png')",
+        'event-1-mobile': "url('/src/images/works/Mobile_1.png')",
+        'event-2-mobile': "url('/src/images/works/Mobile_2.png')",
+        'event-3-mobile': "url('/src/images/works/Mobile_3.png')",
+      }
     },
+
     colors: {
       "deep-blue": "#121926",
       gold: "#FFD971",
       "light-gold": "#FCEDCD",
     },
   },
-  plugins: [],
+  plugins: [
+		require('flowbite/plugin')
+	],
 };
