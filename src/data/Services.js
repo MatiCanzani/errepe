c108053a60957ff38eed3c000e7b5cd5567a5cd7
MatiCanzani <matiascanzani@gmail.com>
2024-01-24 15:21:54 +0000

    add description animation

---
+++ src/data/Services.js
@@ -0,0 +1,40 @@
+const servicesData = [
+  {
+    id: "Service1",
+    title: "EVENTOS",
+    description:
+      "Nuestra experiencia, trayectoria y recursos propios son los que nos hacen capaz de darle servicio de producción gastronómica, mobiliaria y estructural a cualquier tipo de evento, brindando un servicio calidad y garantías.",
+  },
+  {
+    id: "Service2",
+    title: "MOBILIARIOS",
+    description:
+      "Contamos con recursos como mesas de madera para banquetes, salones, mobiliarios para ceremonias, pérgolas y biombos, Chill out, mobiliario de forja, etc. Junto a Star Carpas, brindamos una asesoría de espacialidad y diseño con una vasta trayectoria en Madrid y Buenos Aires.",
+  },
+  {
+    id: "Service3",
+    title: "PRODUCCIÓN",
+    description:
+      "Hacemos lo imposible realidad. Con nuestra alianza española Star Carpas, somos capaces de producir eventos chicos, medianos y masivos, donde sea y cuando sea necesario.",
+  },
+  {
+    id: "Service4",
+    title: "CATERING",
+    description:
+      "Un centro de producción gastronómico propio hace posible que podamos diseñar y producir servicios de catering a medida, con gran reconocimiento en eventos masivos y corporativos.",
+  },
+  {
+    id: "Service5",
+    title: "GASTRONOMÍA",
+    description:
+      "Desde administración a la implementación, brindamos soluciones gastronómicas para comercios, cadenas gastronómicas, eventos corporativos y eventos privados a medida.",
+  },
+  {
+    id: "Service6",
+    title: "CONSULTING",
+    description:
+      "La trayectoria tanto en cadenas gastronómicas como servicios para las principales empresas de Argentina y España, nos posicionan dando valor a nuestros procesos, capaces de capacitar e idear las estructuras para tu próximo proyecto.",
+  },
+];
+
+export default servicesData;
