e2da1342a0e3c45ec8ebfa8dcdba6b23b38fd510
MatiCanzani <matiascanzani@gmail.com>
2024-01-26 15:43:10 +0000

    change tailwind config colors

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -17,22 +17,21 @@ export default {
         FormaDJRMicro: ["FormaDJRMicro", ...defaultTheme.fontFamily.sans],
       },
       backgroundImage: {
-        'event-1-desktop': "url('/src/images/works/Desktop_1.png')",
-        'event-3-desktop': "url('/src/images/works/Desktop_3.png')",
-        'event-2-desktop': "url('/src/images/works/Desktop_2.png')",
-        'event-1-mobile': "url('/src/images/works/Mobile_1.png')",
-        'event-2-mobile': "url('/src/images/works/Mobile_2.png')",
-        'event-3-mobile': "url('/src/images/works/Mobile_3.png')",
-      }
+        "event-1-desktop": "url('/src/images/works/Desktop_1.png')",
+        "event-3-desktop": "url('/src/images/works/Desktop_3.png')",
+        "event-2-desktop": "url('/src/images/works/Desktop_2.png')",
+        "event-1-mobile": "url('/src/images/works/Mobile_1.png')",
+        "event-2-mobile": "url('/src/images/works/Mobile_2.png')",
+        "event-3-mobile": "url('/src/images/works/Mobile_3.png')",
+      },
     },
 
     colors: {
       "deep-blue": "#121926",
-      "golden": "#FFD971",
-      "light-gÍld": "#FCEDCD",
+      gold: "#FFD971",
+      "light-gold": "#FCEDCD",
     },
   },
-  plugins: [
-		require('flowbite/plugin')
-	],
+  plugins: [require("flowbite/plugin")],
 };
+``;
