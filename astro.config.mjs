cd8fafb34d76a46af43471cf8d71fdb510739179
MatiCanzani <matiascanzani@gmail.com>
2025-02-19 14:43:25 +0000

    Update Astro version

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,5 @@
 import { defineConfig } from "astro/config";
-import vercel from "@astrojs/vercel/serverless";
+import vercel from '@astrojs/vercel';
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 
@@ -8,7 +8,7 @@ import icon from "astro-icon";
 // https://astro.build/config
 export default defineConfig({
   site: "https://errepeproductora.com/",
-  output: "hybrid",
+  output: "server",
   adapter: vercel(),
   integrations: [tailwind(), react(), icon()],
   compressHTML: true,
